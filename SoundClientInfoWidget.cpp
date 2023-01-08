723c8ec0343db6d32227fcacbdd197353835c760
gorbatschow <gorbatschow.r@gmail.com>
2023-01-08 20:49:19 +0300

    implemented time holding

--- SoundClientInfoWidget.cpp
+++ SoundClientInfoWidget.cpp
@@ -5,32 +5,60 @@
 void SoundClientInfoWidget::paint() {
   const auto &provider{streamProvider()};
   std::lock_guard(provider.mutex);
+
+  ui.holdTimeCheck.paint();
+  ui.resetHold.paint();
+
+  if (ui.holdTimeCheck.handle()) {
+    _holdTime.exchange(ui.holdTimeCheck());
+  }
+
+  if (ui.resetHold.handle()) {
+    std::for_each(_clients.begin(), _clients.end(),
+                  [](ClientInfo &info) { info.time = 0; });
+  }
+
   if (ImGui::BeginTable("Client Info Table", ClientInfo::NFIELDS)) {
     ImGui::TableSetupColumn("Name");
     ImGui::TableSetupColumn("Type");
     ImGui::TableSetupColumn("Priority");
-    ImGui::TableSetupColumn("Time");
+    ImGui::TableSetupColumn("Time us");
     ImGui::TableHeadersRow();
-    for (const auto &info : _clientInfo) {
-      appendRow(info);
+    for (const auto &info : _clients) {
+      paintRow(info);
     }
     ImGui::EndTable();
   }
 }
 
-void SoundClientInfoWidget::streamDataReady(const RtSoundData &) {
+void SoundClientInfoWidget::updateClientsTable() {
   const auto &provider{streamProvider()};
   std::lock_guard(provider.mutex);
-  _clientInfo.clear();
-  _clientInfo.reserve(provider.clients().size());
-  for (const auto &client : provider.clients()) {
-    const auto &ptr{*client.lock()};
-    _clientInfo.push_back({ptr.clientTypeId(), ptr.clientName(), ptr.priority(),
-                           ptr.streamDataReadyTime()});
+
+  std::for_each(_clients.begin(), _clients.end(),
+                [](ClientInfo &info) { info.exists = false; });
+  for (const auto &clientPtr : provider.clients()) {
+    const auto it{std::find(_clients.begin(), _clients.end(), clientPtr)};
+    const auto &client{*clientPtr.lock()};
+    if (it == _clients.end()) {
+      _clients.push_back({client.clientTypeId(), client.clientId(),
+                          client.clientName(), client.priority(),
+                          client.streamDataReadyTime(), true});
+    } else {
+      auto &info = (*it);
+      info.exists = true;
+      if (_holdTime.load()) {
+        info.time = std::max(client.streamDataReadyTime(), info.time);
+      } else {
+        info.time = client.streamDataReadyTime();
+      }
+    }
   }
+  std::remove_if(_clients.begin(), _clients.end(),
+                 [](const ClientInfo &info) { return info.exists == false; });
 }
 
-void SoundClientInfoWidget::appendRow(const ClientInfo &info) {
+void SoundClientInfoWidget::paintRow(const ClientInfo &info) {
   ImGui::TableNextRow();
   ImGui::TableNextColumn();
   ImGui::TextUnformatted(info.name.c_str());
--- SoundClientInfoWidget.h
+++ SoundClientInfoWidget.h
@@ -1,28 +1,56 @@
 #pragma once
 #include <RtSoundClient.h>
+#include <atomic>
+#include <cstdint>
+#include <imw.h>
+#include <typeindex>
 
 class SoundClientInfoWidget : public RtSoundClient {
 public:
-  SoundClientInfoWidget() = default;
+  SoundClientInfoWidget() { setClientName("SoundClientInfoWidget"); }
   ~SoundClientInfoWidget() = default;
 
   void paint();
 
-  virtual const std::type_info &clientTypeId() const override {
+  const std::type_info &clientTypeId() const override final {
     return typeid(this);
   }
 
 private:
   struct ClientInfo {
-    const std::type_info &typeId;
+    std::type_index typeId;
+    std::uintptr_t cliendId;
     std::string name;
     int priority{};
     long time{};
     bool exists{};
     static inline const int NFIELDS{4};
-  };
-  std::vector<ClientInfo> _clientInfo;
 
-  void streamDataReady(const RtSoundData &) override;
-  void appendRow(const ClientInfo &info);
+    bool operator==(const std::uintptr_t &id) { return (*this).cliendId == id; }
+    bool operator==(const std::weak_ptr<RtSoundClient> &ptr) {
+      return (*this).cliendId == ptr.lock()->clientId();
+    }
+  };
+  std::vector<ClientInfo> _clients;
+
+  void applyStreamConfig(const RtSoundSetup &) override {
+    _clients.clear();
+    _clients.shrink_to_fit();
+    updateClientsTable();
+  }
+  void streamDataReady(const RtSoundData &) override { updateClientsTable(); }
+  void updateClientsTable();
+  void paintRow(const ClientInfo &info);
+
+  struct Ui {
+    Imw::CheckBox holdTimeCheck{"Hold time"};
+    Imw::Button resetHold{"Reset hold"};
+
+    Ui() {
+      resetHold.setSameLine(true);
+      resetHold.setWidth(0.0f);
+    }
+  };
+  Ui ui;
+  std::atomic_bool _holdTime{};
 };
