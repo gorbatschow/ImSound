79dd4d70efbb5dbafe4ca9c8ddd9e3f5e2d3cd11
gorbatschow <gorbatschow.r@gmail.com>
2023-01-14 20:25:28 +0300

    Work on tone generator frequency ui controls

--- ToneGeneratorWidget.h
+++ ToneGeneratorWidget.h
@@ -1,63 +1,43 @@
 #pragma once
+#include "RtSoundSetup.h"
 #include "SoundGeneratorWidget.h"
 #include <RtSoundToneGen.h>
 #include <memory>
 
 class ToneGeneratorWidget : public SoundGeneratorWidget<RtSoundToneGen> {
 public:
+  // Constructor
   ToneGeneratorWidget(std::weak_ptr<RtSoundToneGen> generator_)
       : SoundGeneratorWidget<RtSoundToneGen>(generator_) {}
 
-  ~ToneGeneratorWidget() override = default;
+  // Destructor
+  virtual ~ToneGeneratorWidget() override = default;
 
-  void paint() override final {
-    const auto generator{_generator.lock()};
-    assert(generator != nullptr);
-
-    SoundGeneratorWidget::paint();
-    ui.frequencyModeRadio.paint();
-    if (ui.frequencyModeRadio() == 0) {
-      ui.frequencyPercentSlider.paint();
-    } else if (ui.frequencyModeRadio() == 1) {
-      ui.frequencyIndexSpin.paint();
-    } else {
-      ui.frequencyExactSpin.paint();
-    }
-
-    // ui.frequencyLabel.paint();
-
-    if (ui.frequencyPercentSlider.handle()) {
-      generator->setFrequencyPercent(ui.frequencyPercentSlider());
-      ui.frequencyLabel.setValue(generator->frequencyHertz());
-    }
-  }
+  // Paint
+  virtual void paint() override;
 
 protected:
-  void applyStreamConfig(const RtSoundSetup &setup) override {
-    SoundGeneratorWidget<RtSoundToneGen>::applyStreamConfig(setup);
-
-    auto generator{_generator.lock()};
-    assert(generator);
-
-    generator->setFrequencyPercent(ui.frequencyPercentSlider.value());
-    ui.frequencyLabel.setValue(generator->frequencyHertz());
-  }
+  void applyStreamConfig(const RtSoundSetup &setup) override;
 
 private:
   struct Ui {
-    Imw::Slider<float> frequencyPercentSlider{"Frequency %"};
-    Imw::SpinBox<int> frequencyIndexSpin{"Frequency Idx"};
-    Imw::SpinBox<float> frequencyExactSpin{"Frequency Hz"};
-    Imw::ValueLabel<float> frequencyLabel{"%.2f Hz"};
-    Imw::MultiRadioButton frequencyModeRadio{{"Percent", "Index", "Exact"},
+    Imw::MultiRadioButton frequencyModeRadio{{"Index", "Hertz", "Normal"},
                                              "Frequency Mode"};
+    Imw::SpinBox<int> frequencyIndexSpin{"Frequency Index"};
+    Imw::SpinBox<float> frequencyHertzSpin{"Frequency Hertz"};
+    Imw::ValueLabel<float> frequencyHertzLabel{"Current Frequency %.2f Hz"};
+    Imw::Slider<float> frequencyNormalSlider{"Frequency Normal"};
 
     Ui() {
-      frequencyPercentSlider.setValueLimits({1e-3f, 1e2f});
-      frequencyPercentSlider.setValue(1.0f);
-      frequencyPercentSlider.setTextFormat("%.2f");
-      frequencyExactSpin.setTextFormat("%.2f");
-      // frequencyLabel.setSameLine(true);
+      frequencyIndexSpin.setValueLimits({1, 262144});
+
+      frequencyHertzSpin.setValueLimits({1e-3f, 192e3f});
+      frequencyHertzSpin.setValueStep(100.0f);
+      frequencyHertzSpin.setTextFormat("%.2f");
+
+      frequencyNormalSlider.setValueLimits({1e-3f, 1e2f});
+      frequencyNormalSlider.setValue(1.0f);
+      frequencyNormalSlider.setTextFormat("%.2f");
     }
   };
   Ui ui;
